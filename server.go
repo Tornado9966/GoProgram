48bec8fc2a937faadad39a0ea8c8934c2d0b16e8
Ilya <ilua.mix12@gmail.com>
2019-09-18 21:33:02 +0300

    Add handleStart

--- server.go
+++ server.go
@@ -5,7 +5,8 @@ import (
 )
 
 func handleStart(resWr http.ResponseWriter, req *http.Request) {
-	
+	resWr.Write([]byte("Hello from server!"))
+	fmt.Println("Page success")
 }
 
 func handleCurrentTime(resWr http.ResponseWriter, req *http.Request) {
